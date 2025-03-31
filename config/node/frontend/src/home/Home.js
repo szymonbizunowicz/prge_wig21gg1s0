05c238f3674c92b96d57364a0fd8afa64d14dc1a
szymonbizunowicz <szymon.bizunowicz@student.wat.edu.pl>
2025-03-31 17:28:44 +0200

    wip: frontend home page

---
+++ config/node/frontend/src/home/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Home.css";
+import zdjecie from "./01_mapa_wat.png";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_top">
+        <div> O projekcie</div>
+      </div>
+      <div className="home_bottom">
+        <div className="home_left">
+          <img className="home_logo" src={zdjecie} alt="logo" />
+        </div>
+        <div className="home_right">
+          <h1 className="home_title">
+            <span style={{ color: "#276cd3", opacity: "0.75" }}>GEO</span>Portal
+          </h1>
+          <div className="home_subtitle">
+            Strona interneetowa z interaktywną mapą
+          </div>
+          <button className="home_button">WCHODZĘ!</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
